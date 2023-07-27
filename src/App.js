364245d3d6df14a28c558a14c4f2d218475ba4e3
Burman Bedford <burmanbed@gmail.com>
2023-07-27 13:05:41 -0400

    created button's for the calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 
 
 function App() {
-  return null
+  return (
+    <div className="Calculator-grid">
+      <div className="Output">
+        <div className="Previous-operand"></div>
+        <div className="Current-operand"></div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button>=</button>
+    </div>
+  )
 }
 
 export default App;
